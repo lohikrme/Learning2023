dd5410b11981d7a2e0c1e7e463b007ccb912906e
lohikrme <exitus.rabies@gmail.com>
2023-06-10 22:00:51 +0300

    all works now 10.6.2023 lohikrme

--- harjoitus2/datascience.js
+++ harjoitus2/datascience.js
@@ -53,14 +53,18 @@ d3.json("measures.json").then(
                 month: d.key,
                 airPressure: d3.mean(d.values, function (v) { return v.AirPressure }),
                 humidity: d3.mean(d.values, function (v) { return v.Humidity }),
-                temp: d3.mean(d.values, function (v) { return v.Temp })
+                temperature: d3.mean(d.values, function (v) { return v.Temp })
             }
         })
 
         // use the monthlyAverage array for your visualization
         console.log(monthlyAverage)
 
-        // start creating svg graphics
+        //********************************/
+        // START CREATING SVG GRAPHICS
+        //********************************
+
+        // first give basic dimensions for all separate svg-graphics
         var graph_width = 300
         var graph_height = 300
 
@@ -71,7 +75,7 @@ d3.json("measures.json").then(
             left: 50
         }
 
-        // start creating the airpressure graphic
+        // then start creating the airpressure-graphic
         var svg_airpressure = d3.select("#airpressure-graph")
             .attr("width", graph_width + margin.left + margin.right)
             .attr("height", graph_height + margin.top + margin.bottom)
@@ -93,7 +97,7 @@ d3.json("measures.json").then(
             .padding(0.1)
 
         var y_airpressure = d3.scaleLinear()
-            .domain([0, d3.max(monthlyAverage, item => item.airPressure)])
+            .domain([940, d3.max(monthlyAverage, item => item.airPressure) + 40])
             .range([graph_height, 0])
 
         svg_airpressure.selectAll(".bar")
@@ -106,14 +110,121 @@ d3.json("measures.json").then(
             .attr("height", item => graph_height - y_airpressure(item.airPressure))
             .attr("fill", "teal")
 
-        // add x-axis and y-axis to airpressure graphic
-
         var Y_Axis_AirPressure = d3.axisLeft(y_airpressure).tickFormat(d3.format("d"))
-
         svg_airpressure.append("g")
             .attr("class", "y-axis")
             .call(Y_Axis_AirPressure)
 
+
+        var X_Axis_Airpressure = d3.axisBottom(x_airpressure);
+        svg_airpressure.append("g")
+            .attr("class", "x axis")
+            .attr("transform", "translate(0," + graph_height + ")")
+            .call(X_Axis_Airpressure)
+            .selectAll("text")
+            .style("text-anchor", "center")
+            .attr("dy", "1em")
+
+
+
+        // start creating humidity graph
+        var svg_humidity = d3.select("#humidity-graph")
+            .attr("width", graph_width + margin.left + margin.right)
+            .attr("height", graph_height + margin.top + margin.bottom)
+            .append("g")
+            .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
+
+        svg_humidity.append("text")
+            .attr("x", (graph_width / 2))
+            .attr("y", 0 - (margin.top / 2))
+            .attr("text-anchor", "middle")
+            .style("font-size", "16px")
+            .style("font-weight", "bold")
+            .style("font-family", "Arial")
+            .text("Kuukausittainen ilmankosteuden keskiarvo")
+
+        var x_humidity = d3.scaleBand()
+            .domain(monthlyAverage.map(item => item.month))
+            .range([0, graph_width])
+            .padding(0.1)
+
+        var y_humidity = d3.scaleLinear()
+            .domain([0, d3.max(monthlyAverage, item => item.humidity + 20)])
+            .range([graph_height, 0])
+
+        svg_humidity.selectAll(".bar")
+            .data(monthlyAverage)
+            .enter().append("rect")
+            .attr("class", "bar")
+            .attr("x", item => x_humidity(item.month))
+            .attr("y", item => y_humidity(item.humidity))
+            .attr("width", x_humidity.bandwidth())
+            .attr("height", item => graph_height - y_humidity(item.humidity))
+            .attr("fill", "LightBlue")
+
+        var Y_Axis_Humidity = d3.axisLeft(y_humidity).tickFormat(d3.format("d"))
+        svg_humidity.append("g")
+            .attr("class", "y-axis")
+            .call(Y_Axis_Humidity)
+
+        var X_Axis_Humidity = d3.axisBottom(x_humidity);
+        svg_humidity.append("g")
+            .attr("class", "x axis")
+            .attr("transform", "translate(0," + graph_height + ")")
+            .call(X_Axis_Humidity)
+            .selectAll("text")
+            .style("text-anchor", "center")
+            .attr("dy", "1em")
+
+        // start creating temperature graph
+
+        var svg_temperature = d3.select("#temperature-graph")
+            .attr("width", graph_width + margin.left + margin.right)
+            .attr("height", graph_height + margin.top + margin.bottom)
+            .append("g")
+            .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
+
+        svg_temperature.append("text")
+            .attr("x", (graph_width / 2))
+            .attr("y", 0 - (margin.top / 2))
+            .attr("text-anchor", "middle")
+            .style("font-size", "16px")
+            .style("font-weight", "bold")
+            .style("font-family", "Arial")
+            .text("Kuukausittainen lämpötilan keskiarvo")
+
+        var x_temperature = d3.scaleBand()
+            .domain(monthlyAverage.map(item => item.month))
+            .range([0, graph_width])
+            .padding(0.1)
+
+        var y_temperature = d3.scaleLinear()
+            .domain([20, d3.max(monthlyAverage, item => item.temperature + 5)])
+            .range([graph_height, 0])
+
+        svg_temperature.selectAll(".bar")
+            .data(monthlyAverage)
+            .enter().append("rect")
+            .attr("class", "bar")
+            .attr("x", item => x_temperature(item.month))
+            .attr("y", item => y_temperature(item.temperature))
+            .attr("width", x_temperature.bandwidth())
+            .attr("height", item => graph_height - y_temperature(item.temperature))
+            .attr("fill", "orange")
+
+        var Y_Axis_Temperature = d3.axisLeft(y_temperature).tickFormat(d3.format("d"))
+        svg_temperature.append("g")
+            .attr("class", "y-axis")
+            .call(Y_Axis_Temperature)
+
+        var X_Axis_Temperature = d3.axisBottom(x_temperature);
+        svg_temperature.append("g")
+            .attr("class", "x axis")
+            .attr("transform", "translate(0," + graph_height + ")")
+            .call(X_Axis_Temperature)
+            .selectAll("text")
+            .style("text-anchor", "center")
+            .attr("dy", "1em")
     }
 
 )
